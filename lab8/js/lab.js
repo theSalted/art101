ebfdd58c20ea0f483f4c5cec935b2f6cf051d82e
ychen514 <ychen514@ucsc.edu>
2021-02-09 03:25:19 +0000

    Lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,45 @@
+/*
+ * Author: Yuhao Chen <ychen514@ucsc.edu>
+ * Created: 9 February
+ * License: Public Domain
+ */
+
+function firstThing(test) {
+  console.log(test + ": This is the first thing. <br>")
+}
+
+function secondThing(test) {
+  console.log(test + ": This is the second thing. <br>")
+}
+
+function thirdThing(test) {
+  console.log(test + ": This is the first thing. <br>")
+}
+
+firstThing("TEST 1")
+secondThing("TEST 1")
+thirdThing("Test 1")
+
+setTimeout(function() {
+  firstThing("TEST 2")
+}, 0)
+
+setTimeout(function() {
+  secondThing("TEST 2")
+}, 0)
+
+setTimeout(function() {
+  thirdThing("TEST 2")
+}, 2000)
+
+setTimeout(function() {
+  firstThing("TEST 3")
+}, 2000)
+
+setTimeout(function() {
+  secondThing("TEST 3")
+}, 2000)
+
+setTimeout(function() {
+  thirdThing("TEST 3")
+}, 2000)
