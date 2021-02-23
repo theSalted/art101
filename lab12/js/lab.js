12ff098a3425e6658d9401ca03bd2eefa44f5d88
ychen514 <ychen514@ucsc.edu>
2021-02-23 03:34:15 +0000

    Lab 11

---
+++ lab12/js/lab.js
@@ -0,0 +1,27 @@
+/*
+ * Author: Yuhao Chen <ychen514@ucsc.edu>
+ * Created: 23 February
+ * License: Public Domain
+ */
+
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor";
+  } else if (mod == 1) {
+    return "Ravenclaw";
+  } else if (mod == 2) {
+    return "Slytherin";
+  } else if (mod == 3) {
+    return "Hufflepuff";
+  }
+}
+
+var myButtton = document.getElementById("button");
+myButtton.addEventListener("click", function () {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+});
